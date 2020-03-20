91c468d7c9c6e853f6bc9cbe04aa5092577d0a5e
Connor Monahan <cmonahan@innovationdx.com>
2020-03-20 00:17:34 -0500

    Disabled by default

--- consent/static/script.js
+++ consent/static/script.js
@@ -1,14 +1,31 @@
-window.addEventListener('load', (event) => {
-    document.getElementById("id_ability_to_consent").addEventListener('change', function() {
-        if(document.getElementById('id_ability_to_consent').value == 1) {
-            document.getElementById('id_inability_reason').disabled = true;
-            document.getElementById('id_representative_name').disabled = true;
-            document.getElementById('id_relationship_to_patient').disabled = true;
-            document.getElementById('id_relationship_to_patient').value = "";
+function disable()
+{
+    document.getElementById('id_inability_reason').disabled = true;
+    document.getElementById('id_representative_name').disabled = true;
+    document.getElementById('id_relationship_to_patient').disabled = true;
+    document.getElementById('id_inability_reason').value = "";
+    document.getElementById('id_representative_name').value = "";
+    document.getElementById('id_relationship_to_patient').value = "";
+}
+function enable()
+{
+    document.getElementById('id_inability_reason').disabled = false;
+    document.getElementById('id_representative_name').disabled = false;
+    document.getElementById('id_relationship_to_patient').disabled = false;
+}
+
+window.addEventListener('load', (event) =>
+{
+    disable();
+    document.getElementById("id_ability_to_consent").addEventListener('change', () =>
+    {
+        if (document.getElementById('id_ability_to_consent').value == 1)
+        {
+            disable();
         }
-        else{
-            document.getElementById('id_inability_reason').disabled = false;
-            document.getElementById('id_representative_name').disabled = false;
-            document.getElementById('id_relationship_to_patient').disabled = false;
-        }});
+        else
+        {
+            enable();
+        }
+    });
 });
