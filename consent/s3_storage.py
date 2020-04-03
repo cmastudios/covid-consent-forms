7048b2b3ae2efd6b362ee37171f7ba7476c9d975
Connor Monahan <cmonahan@innovationdx.com>
2020-04-03 18:50:14 -0500

    Add S3 security settings

--- consent/s3_storage.py
+++ consent/s3_storage.py
@@ -2,28 +2,16 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-
     bucket_name = 'consentformmedia.innovationdx.com'
-
-    def path(self, name):
-        pass
-
-    def get_accessed_time(self, name):
-        pass
-
-    def get_created_time(self, name):
-        pass
+    region_name = 'us-west-2'
+    querystring_auth = True
+    querystring_expire = 600  # expire after 10 minutes for security
+    object_parameters = {'ServerSideEncryption': 'AES256'}
+    default_acl = None
 
 
 class StaticStorage(S3Boto3Storage):
-
     bucket_name = 'consentformstatic.innovationdx.com'
-
-    def path(self, name):
-        pass
-
-    def get_accessed_time(self, name):
-        pass
-
-    def get_created_time(self, name):
-        pass
+    region_name = 'us-west-2'
+    querystring_auth = False
+    default_acl = 'public-read'
